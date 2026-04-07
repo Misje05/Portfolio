83a19afe405e6e843933bcbf781a38df1cb03c91
184938 <magnusmisje05@gmail.com>
2026-04-07 11:39:51 +0200

    configuration for lokal and github hosting in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Portfolio/">
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Portfolio/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
